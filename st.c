7d644d1da4ebfa57332cb4317b4fbb032d4acd0f
Christopher Parks <christopher.parks@protonmail.com>
2023-11-03 08:27:53 -0500

    Unhide cursor on RIS (\033c)

--- st.c
+++ st.c
@@ -2584,6 +2584,7 @@ eschandle(uchar ascii)
 		treset();
 		resettitle();
 		xloadcols();
+		xsetmode(0, MODE_HIDE);
 		break;
 	case '=': /* DECPAM -- Application keypad */
 		xsetmode(1, MODE_APPKEYPAD);
